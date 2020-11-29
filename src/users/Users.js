5c6cef4df07ada2a0ba831d18bbd5de9341a84da
Wahid-Mujawar <abdulwahid0218@gmail.com>
2020-11-28 18:40:24 -0800

    test

--- src/users/Users.js
+++ src/users/Users.js
@@ -4,9 +4,13 @@ import User from './User'
 class Users extends Component {
 render () {
     return (<div> <h2>Users</h2><h2/>
-           <User age="22">Wahid</User>
-            <User age="22">Vinay</User>
-            <User age="22">Abhi</User>
+        <User age="22">Wahid</User>
+        <User age="22">Vinay</User>
+        <User age="23">Abhi</User>
+        <User age="29">John</User>
+        <User age="32">Tom</User>
+        <User age="36">Jerry</User>
+
     </div>)
     
 }
